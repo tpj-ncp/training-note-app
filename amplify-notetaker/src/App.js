8520f6c4f3ff7f269a51ac3d8d2a1e4a35d3e776
Tyler Jewell <tpj.ncp@gmail.com>
2020-10-06 12:33:53 -0500

    first commit after working aut

---
+++ amplify-notetaker/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div>App</div>
+  );
+}
+
+export default withAuthenticator(App);
